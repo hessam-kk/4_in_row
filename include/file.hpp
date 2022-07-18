03a9d1f723610994488b3d090a5b2af659ec96c8
mohammadgit2000 <mohammadgit7299@gmail.com>
2022-07-18 14:41:04 +0100

    add write to matrix function

--- include/file.hpp
+++ include/file.hpp
@@ -25,11 +25,14 @@ unsigned short int return_color_number(enum Color color)
 
         case BLUE:
             return 3;
+
+        case EMPTY :
+            return 4;
     }
 }
 
 
-void Write_To_File(unsigned short int all_moves , enum Color player_1 , enum Color player_2)
+void Write_Player_Info_To_File(unsigned short int all_moves , enum Color player_1 , enum Color player_2)
 {
     std::ofstream file;
     file.open("Player.txt" , std::ios::out);
@@ -44,10 +47,35 @@ void Write_To_File(unsigned short int all_moves , enum Color player_1 , enum Col
     }
     
     file << "moves : " << all_moves << std::endl;
-    
+
     file << "player_1 : " << return_color_number(player_1) << std::endl;
 
     file << "player_2 : " << return_color_number(player_2) << std::endl;
 
     file.close();
+}
+
+
+void Write_Matrix_To_File(enum Color matrix_8_8[8][8])
+{
+    std::ofstream file;
+    file.open("Matrix.txt" , std::ios::out);
+
+    if (! file)
+    {
+        std::cerr << "Error opening file" << std::endl;
+        std::cerr << "Enter Any Key To Exit" << std::endl;
+        std::cin.ignore();
+        getchar();
+    }
+
+    for (int i = 0 ; i < 8 ; i++)
+    {
+        for (int j = 0 ; j < 8 ; j++)
+        {
+            file << return_color_number(matrix_8_8[i][j]) << " ";
+        }
+
+        file << std::endl;
+    }
 }
