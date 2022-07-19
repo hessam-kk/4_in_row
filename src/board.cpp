71ab2d14afd22e4da11ad45ca9a7449bb577a1ae
mohammadgit2000 <mohammadgit7299@gmail.com>
2022-07-19 09:55:43 +0100

    added -1 to column in matrix index

--- src/board.cpp
+++ src/board.cpp
@@ -2,18 +2,19 @@
 #include "board.hpp"
 #include <iostream>
 #include <string>
+
 int insert_piece(const int column,
                 const enum Color color,
                 enum Color board[8][8])
 {
     // wrong column number
-    if(column < 0 || column > 7){
+    if(column - 1 < 0 || column - 1 > 7){
         std::cout << "Error on insert: wrong column number" << std::endl;
         return -1;
     }
     
     // column is full
-    if (board[column][0] != EMPTY){
+    if (board[column - 1][0] != EMPTY){
         std::cout << "Error on insert: Column is full" << std::endl;
         return -1;
     }
@@ -21,10 +22,10 @@ int insert_piece(const int column,
 
     for(int i = 7; i >= 0; i--)
     {
-        if(board[column][i] == EMPTY)
+        if(board[column - 1][i] == EMPTY)
         {
             std::cout << "insert on " << i << std::endl;
-            board[column][i] = color;
+            board[column - 1][i] = color;
             return i;
         }
     }
@@ -68,30 +69,39 @@ int convert_string_to_int(const char str)
     case '1':
         num = 1;
         break;
+
     case '2':
         num = 2;
         break;
+
     case '3':
         num = 3;
         break;
+
     case '4':
         num = 4;
         break;
+
     case '5':
         num = 5;
         break;
+
     case '6':
         num = 6;
         break;
+
     case '7':
         num = 7;
         break;
+
     case '8':
         num = 8;
         break;
+
     default:
         num = -1;
         break;
     }
+
     return num;
 }
